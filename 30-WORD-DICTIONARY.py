9079a92dad1e000dd8ea11bbf6d598bee959c4ad
knnrae <karenannperez999@gmail.com>
2025-02-16 22:03:08 +0800

    Add files via upload

---
+++ 30-WORD-DICTIONARY.py
@@ -0,0 +1,41 @@
+DATA = {
+    "REDNECK": "insensitive person",
+    "GOLDEN RULE": "a wise rule",
+    "AIRY-FAIRY": "lofty and impractical",
+    "WATERSHED": "a decisive turning point",
+    "DOWN-TO-EARTH": "practical and sensible",
+    "STICK-IN-THE-MUD": "someone wihtout initiative who never takes a chance",
+    "TIP OF THE ICEBERG": "evidence that a great deal more exists but remains hidden",
+    "WINDBAG": "someone who is talkative but incapable of action",
+    "TIME-CONSUMING": "requiring a great deal of time",
+    "ON THE SPUR OF THE MOMENT": "on a sudden impulse, without premedidation",
+    "LET BY-GONES BE BY-GONES": "let's forgive and forget",
+    "ALIVE AND KICKING": "very much alive",
+    "SICK TO DEATH": "exasperated beyond endurance",
+    "LIKE AN ANGEL": "with the utmost innocence and purity",
+    "BED OF ROSES": "a pleasant, enjoyable situation without any drawbacks and irritations",
+    "PRIMROSE PATH": "the path of self-indulgence that ends in ruin",
+    "LOTUS-EATERS": "people who neglects their friends and families in order to enjoy a life of luxury and idleness",
+    "PICK OF THE BUNCH": "the outstanding one in a group",
+    "HIT THE HAY": "to go to bed",
+    "LAST STRAW": "beyond limit; the breaking point",
+    "GO HAYWIRE": "to go crazy",
+    "SLEEP LIKE A DOG": "to sleep soundly",
+    "LAP-DOG": "to be pampered and overprotected",
+    "LOOK SHEEPISH": "to look embarrassed or guilty",
+    "SCAPEGOAT": "to be punished for the sins and crime of others",
+    "PIG-HEADED": "stubborn",
+    "MOLE": "a traitor who undermines from the inside the organization in which he is employed",
+    "SCOTCH THE SNAKE": "to spoil a plan",
+    "MONKEY BUSINESS": "underhanded business",
+    "LION'S DEN": "a place of great danger"
+    }
+
+a=input("What english idiom?")
+print("Meaning:" , DATA[a])
+    
+    
+    
+    
+    
+    
